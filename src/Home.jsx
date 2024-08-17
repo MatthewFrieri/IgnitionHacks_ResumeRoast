95b511461c3f950d15562d33c88a51a3beecfcbd
NickrodBasiri <basirinickrod@gmail.com>
2024-08-17 13:11:33 -0400

    edited spotlight fixed buttons not working

--- src/Home.jsx
+++ src/Home.jsx
@@ -26,14 +26,16 @@ export default function Home() {
   };
 
   return (
-    <div className="relative -z-20 flex flex-col justify-center items-center bg-gradient-to-b from-zinc-800 to-zinc-900 w-screen h-screen overflow-hidden">
-      <div className="-z-10 absolute border-x-[500px] border-x-transparent border-yellow-100 border-b-[3000px] -rotate-[60deg]"></div>
-      <div className="top-20 left-10 absolute border-2 border-red-500">
-        <h1 className="text-4xl">Resume Roaster</h1>
-        <input type="file" onChange={onFileChange} className="my-20" />
-        <button onClick={onFileSubmit} className="bg-white w-20 h-10">
-          Roast Me
-        </button>
+    <div className="relative flex flex-col justify-center items-center bg-gradient-to-b from-zinc-800 to-zinc-900 w-screen h-screen overflow-hidden">
+      <div className="absolute border-x-[400px] border-x-transparent border-yellow-100 border-b-[3000px] -rotate-[60deg]"></div>
+      <div className="top-[250px] left-[400px] z-10 absolute border-2 border-red-500">
+        <h1 className="text-7xl">Resume Roaster</h1>
+        <div className="flex justify-between border-2 border-red-700">
+          <input type="file" onChange={onFileChange} className="bg-red-500"/>
+          <button onClick={onFileSubmit} className="bg-white w-20 h-10">
+            Roast Me
+          </button>
+        </div>
       </div>
     </div>
   );
