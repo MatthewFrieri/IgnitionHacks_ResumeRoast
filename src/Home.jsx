ee9f07bb4f36cba7866cacf5579fa350c009aab9
NickrodBasiri <basirinickrod@gmail.com>
2024-08-17 15:35:57 -0400

    fixed ui on 100% zoom.

--- src/Home.jsx
+++ src/Home.jsx
@@ -27,19 +27,19 @@ export default function Home() {
 
   return (
     <div className="relative flex flex-col justify-center items-center bg-gradient-to-b from-zinc-700 to-zinc-900 w-screen h-screen overflow-hidden">
-      <div className="absolute right-1 border-x-[600px] border-x-transparent border-yellow-100 border-b-[3000px] -rotate-[20deg]"></div>
+      <div className="absolute right-1 border-x-[400px] border-x-transparent border-yellow-100 border-b-[3000px] -rotate-[20deg]"></div>
       <div className="flex flex-col gap-8 top-[100px] left-[150px] z-10 absolute">
         <div>
-          <h1 className="text-[175px] text-left text-gray-200 leading-[150px]">Resume <br/> Roast</h1>
-          <h1 className="text-4xl text-left text-gray-200 ml-2">This is a cool line or something!</h1>
+          <h1 className="text-[150px] text-left text-gray-200 leading-[150px]">Resume <br/> Roast</h1>
+          <h1 className="text-3xl text-left text-gray-200 ml-2 mt-5">This is a cool line or something!</h1>
         </div>
-        <div className="flex flex-row items-center gap-8 mt-20">
+        <div className="flex flex-row items-center gap-8">
           <label for="fileInput" class="customFileInput">
             <span>{file ? <p>{file.name}</p> : <p>Select File</p>}</span>
             </label>
           <input type="file" id="fileInput" onChange={onFileChange} className="">
           </input>
-          <button onClick={onFileSubmit} className="text-4xl w-[250px] h-[128px] border-2 border-blue-500 rounded-[4px] text-white text-[48px]">
+          <button onClick={onFileSubmit} className="text-4xl w-[300px] h-[90px] border-2 border-blue-500 rounded-[4px] text-white text-[48px]">
             Roast Me
           </button>
         </div>
